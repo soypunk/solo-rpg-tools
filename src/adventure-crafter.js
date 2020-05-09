4b3b86e5668453d8e90f75aca1e885bc281a5117
Shawn Medero <soypunk@users.noreply.github.com>
2020-05-09 14:38:23 -0700

    added an array compare routine to help with changing AC themes

--- src/adventure-crafter.js
+++ src/adventure-crafter.js
@@ -5,18 +5,43 @@ const utils = new Utils()
 export default class AdventureCrafter {
 
     constructor() {
-        this._themes = ['Action','Tension','Mystery','Social','Personal']
+        this._baseThemes = ['Action','Tension','Mystery','Social','Personal']
+        this._themes = this._baseThemes
         this._themesTable = [
-        [2,"Action"],
-        [4,"Tension"],
-        [6,"Mystery"],
-        [8,"Social"],
-        [11,"Personal"]]
+        [2,this.themes[0]],
+        [4,this.themes[1]],
+        [6,this.themes[2]],
+        [8,this.themes[3]],
+        [11,this.themes[4]]]
+    }
+
+    get baseThemes() {
+        return this._baseThemes
     }
     
     get themes() {
         return this._themes
     }
+    
+    set initThemesTable() {
+        this._themesTable = [
+        [2,this.themes[0]],
+        [4,this.themes[1]],
+        [6,this.themes[2]],
+        [8,this.themes[3]],
+        [11,this.themes[4]]]
+    }
+    
+    set themes(themes) {
+        let isProperThemeArray = areArraysEqualSets(themes,this.baseThemes)
+        
+        if(isProperThemeArray) {
+            this._themes = themes
+            this.initThemesTable()
+        } else {
+            throw "Your theme array contains an invalid set of Adventure Crafter themes. Custom themes are not supported."
+        }
+    }    
     
     get themesTable() {
         return this._themesTable
@@ -26,7 +51,6 @@ export default class AdventureCrafter {
 		let diceResult = utils.rollDice(1,10)
         let result = utils.getClosetRolledMatch(this.themesTable,diceResult.total)
         
-
         return {
             "result": result,
             "dice": {
--- src/utils.js
+++ src/utils.js
@@ -91,5 +91,29 @@ export default class Utils {
 	
     numberWithCommas(x) {
         return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
-    }	
+    }
+    
+	areArraysEqualSets(a1, a2) {
+	  let superSet = {};
+	  for (let i = 0; i < a1.length; i++) {
+	    const e = a1[i] + typeof a1[i];
+	    superSet[e] = 1;
+	  }
+
+	  for (let i = 0; i < a2.length; i++) {
+	    const e = a2[i] + typeof a2[i];
+	    if (!superSet[e]) {
+	      return false;
+	    }
+	    superSet[e] = 2;
+	  }
+
+	  for (let e in superSet) {
+	    if (superSet[e] === 1) {
+	      return false;
+	    }
+	  }
+
+	  return true;
+	}    
 }
